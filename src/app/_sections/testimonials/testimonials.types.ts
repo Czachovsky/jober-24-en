986321d9880c780af0a9fe7cddcab3cffc5ef538
Marcin Rajchel <rajchel.marcin@gmail.com>
2025-03-12 10:03:47 +0100

    test

---
+++ src/app/_sections/testimonials/testimonials.types.ts
@@ -0,0 +1,32 @@
+export interface TestimonialsTypes {
+  score: number;
+  header: string;
+  opinion: string;
+  name: string;
+  company: string;
+}
+
+export const testimonials: TestimonialsTypes[] = [
+  {
+    "score": 5,
+    "header": "110 people in 10 days – a lifesaver for the company at the start of the summer season",
+    "opinion": "The summer season was approaching, and a service company was facing the problem of staff shortages – vacations of permanent employees and the ineffectiveness of other agencies meant that there was no way to serve all clients. Immediate action was needed, but competing agencies were only providing individual candidates. Thanks to Jober24, a complete 110-person team was built in 10 days.",
+    "name": "Anna N.",
+    "company": "Roldrob"
+  },
+  {
+    "score": 5,
+    "header": "18 aviation engineers in 14 days – express recruitment for an industry leader",
+    "opinion": "A global manufacturer of aerospace components faced an urgent challenge – it urgently needed 18 highly skilled engineers to keep up with the growing demand and meet deadlines for key contracts. Thanks to Jober24 and our 24/7 global recruitment network, we delivered a complete team of engineers specializing in aerodynamics, avionics, and composites in just 14 days.",
+    "name": "Marek K.",
+    "company": "AIRBUS"
+  },
+  {
+    "score": 5,
+    "header": "50 people in 7 days – express recruitment for e-commerce during peak sales",
+    "opinion": "A rapidly growing e-commerce store faced the challenge of a sudden increase in orders during the sales season. The lack of additional hands to work threatened delays in order fulfillment and customer dissatisfaction. Thanks to Jober24 and our lightning-fast 24/7 recruitment, we delivered 50 qualified warehouse and logistics workers ready to take immediate action in just 7 days.",
+    "name": "Jan W.",
+    "company": "Logistic"
+  }
+]
+
